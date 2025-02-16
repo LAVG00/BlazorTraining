9c6c588876221f6eb5c90709fe174d792aad7000
LAVG00 <lavg8759@gmail.com>
2025-02-16 17:37:01 -0600

    created client

---
+++ BlazorTraining/Clients/GamesClient.cs
@@ -0,0 +1,33 @@
+﻿using BlazorTraining.Models;
+
+namespace BlazorTraining.Clients
+{
+    public class GamesClient
+    {
+        private readonly GameSummary[] games = [
+            new(){
+                Id = 1,
+                Name ="Dark souls",
+                Genre = "RPG",
+                Price = 549.00M,
+                ReleaseDate = new DateOnly(2011, 9, 22)
+            },
+            new(){
+                Id = 2,
+                Name = "Pokemon Platinum",
+                Genre = "RPG",
+                Price = 800.00M,
+                ReleaseDate = new DateOnly(2008, 9, 13)
+            },
+            new(){
+                Id= 3,
+                Name = "Monster Hunter Rise",
+                Genre = "ARPG",
+                Price = 490.00M,
+                ReleaseDate = new DateOnly(2021, 3, 26)
+            }
+        ];
+
+        public GameSummary[] GetGames() => [.. games];
+    }
+}
