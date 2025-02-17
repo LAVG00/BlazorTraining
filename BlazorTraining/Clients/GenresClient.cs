c5cbce0505beac120483594105984d06c63bc8b0
LAVG00 <lavg8759@gmail.com>
2025-02-16 21:26:25 -0600

    added editgame component

---
+++ BlazorTraining/Clients/GenresClient.cs
@@ -0,0 +1,27 @@
+﻿using BlazorTraining.Models;
+
+namespace BlazorTraining.Clients
+{
+    public class GenresClient
+    {
+        private readonly Genre[] genres = [
+            new(){
+                Id = 1,
+                Name = "Fighting"
+            },
+            new(){
+                Id = 2,
+                Name="RPG"
+            },
+            new(){
+                Id = 3,
+                Name ="ARPG"
+            }, new(){
+                Id = 4,
+                Name ="FPS"
+            }
+        ];
+
+        public Genre[] GetGenres() => genres;
+    }
+}
